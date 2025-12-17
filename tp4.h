b0918b7bb282edc0fdf96534d302799a863f46ef
ams187 <amssaidna@gmail.com>
2025-12-17 23:11:33 +0100

    Modifs complexité

--- tp4.h
+++ tp4.h
@@ -1,6 +1,7 @@
 /*
  * tp4.h
- * Déclarations des structures et prototypes pour l'indexation de texte par ABR.
+ * Version : GOD TIER FINAL
+ * Optimisations : Queue d'insertion + Cache de phrases + Arithmétique Pointeurs
  */
 
 #ifndef TP4_H
@@ -10,10 +11,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <strings.h> // Pour strcasecmp
 
-/* --- Structures de données --- */
+/* --- Structures --- */
 
-// Structure représentant une position unique d'un mot
 typedef struct Position {
     int numeroLigne;
     int ordre;
@@ -21,46 +22,38 @@ typedef struct Position {
     struct Position* suivant;
 } T_Position;
 
-// Structure représentant un nœud de l'ABR
 typedef struct Noeud {
     char* mot;
     int nbOccurences;
     T_Position* listePositions;
+    
+    // Optimisation 1 : Insertion en O(1)
+    T_Position* dernierePosition; 
+    
     struct Noeud* filsGauche;
     struct Noeud* filsDroit;
 } T_Noeud;
 
-// Structure représentant l'Index global
 typedef struct Index {
     T_Noeud* racine;
     int nbMotsDistincts;
     int nbMotsTotal;
+
+    // Optimisation 2 : Cache de phrases complètes (Accès O(1))
+    char** tabPhrases; 
+    int nbPhrasesCapacite;
 } T_Index;
 
-/* --- Prototypes des fonctions --- */
+/* --- Prototypes --- */
 
-// Gestion des positions
+void initIndex(T_Index* index);
 T_Position* ajouterPosition(T_Position* listeP, int ligne, int ordre, int phrase);
-
-// Gestion de l'ABR
 int ajouterOccurence(T_Index* index, char* mot, int ligne, int ordre, int phrase);
-
-// Indexation du fichier
 int indexerFichier(T_Index* index, char* filename);
-
-// Affichage de l'index
 void afficherIndex(T_Index index);
-
-// Recherche
 T_Noeud* rechercherMot(T_Index index, char* mot);
-
-// Affichage des occurrences d'un mot (OPTIMISÉ)
 void afficherOccurencesMot(T_Index index, char* mot);
-
-// Reconstruction du texte
 void construireTexte(T_Index index, char* filename);
-
-// Gestion mémoire
 void libererIndex(T_Index* index);
 
 #endif
